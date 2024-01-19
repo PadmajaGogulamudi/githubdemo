9a45597a595ef1667388b771df4686fdca94c585
PadmajaGogulamudi <117999437+PadmajaGogulamudi@users.noreply.github.com>
2024-01-19 16:42:43 +0530

    Add files via upload

---
+++ codewars8.py
@@ -0,0 +1,19 @@
+def all_continents(lst): 
+    l=list(('Africa','Americas','Asia','Europe','Oceania'))
+    for i in lst:
+        for j in i:
+            if j=='continent':
+                if i['continent'] in l:
+                    l.remove(i['continent'])
+    if len(l)==0:
+        return True
+    else:
+        return False
+list1 =  [
+  { 'firstName': 'Fatima', 'lastName': 'A.', 'country': 'Algeria', 'continent': 'Africa', 'age': 25, 'language': 'JavaScript' },
+  { 'firstName': 'Agustín', 'lastName': 'M.', 'country': 'Chile', 'continent': 'Americas', 'age': 37, 'language': 'C' },
+  { 'firstName': 'Jing', 'lastName': 'X.', 'country': 'China', 'continent': 'Asia', 'age': 39, 'language': 'Ruby' },
+  { 'firstName': 'Laia', 'lastName': 'P.', 'country': 'Andorra', 'continent': 'Europe', 'age': 55, 'language': 'Ruby' },
+  { 'firstName': 'Oliver', 'lastName': 'Q.', 'country': 'Australia', 'continent': 'Oceania', 'age': 65, 'language': 'PHP' }
+  ]
+print(all_continents(list1))
