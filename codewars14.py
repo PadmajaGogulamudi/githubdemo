afabce0c4454fc940397811b5e16f57d0dc4e5c4
PadmajaGogulamudi <117999437+PadmajaGogulamudi@users.noreply.github.com>
2024-01-19 16:45:22 +0530

    Add files via upload

---
+++ codewars14.py
@@ -0,0 +1,22 @@
+def order_food(lst): 
+    meal_count = {}
+
+    for participant in lst:
+        meal_option = participant['meal']
+        if meal_option in meal_count:
+            meal_count[meal_option] += 1
+        else:
+            meal_count[meal_option] = 1
+
+    return meal_count
+
+# Example usage with the provided list1
+list1 = [
+    { 'firstName': 'Noah', 'lastName': 'M.', 'country': 'Switzerland', 'continent': 'Europe', 'age': 19, 'language': 'C', 'meal': 'vegetarian' },
+    { 'firstName': 'Anna', 'lastName': 'R.', 'country': 'Liechtenstein', 'continent': 'Europe', 'age': 52, 'language': 'JavaScript', 'meal': 'standard' },
+    { 'firstName': 'Ramona', 'lastName': 'R.', 'country': 'Paraguay', 'continent': 'Americas', 'age': 29, 'language': 'Ruby', 'meal': 'vegan' },
+    { 'firstName': 'George', 'lastName': 'B.', 'country': 'England', 'continent': 'Europe', 'age': 81, 'language': 'C', 'meal': 'vegetarian' },
+]
+
+result = order_food(list1)
+print(result)
