b7897f6abd114937244f7057280e700dcd095b5d
PadmajaGogulamudi <117999437+PadmajaGogulamudi@users.noreply.github.com>
2024-01-19 16:42:27 +0530

    Add files via upload

---
+++ codewars7.py
@@ -0,0 +1,11 @@
+def find_senior(lst):
+    x = max(dev['age'] for dev in lst)
+    y = [dev for dev in lst if dev['age'] == x]
+    return y
+list1 = [
+  { 'firstName': 'Gabriel', 'lastName': 'X.', 'country': 'Monaco', 'continent': 'Europe', 'age': 49, 'language': 'PHP' },
+  { 'firstName': 'Odval', 'lastName': 'F.', 'country': 'Mongolia', 'continent': 'Asia', 'age': 38, 'language': 'Python' },
+  { 'firstName': 'Emilija', 'lastName': 'S.', 'country': 'Lithuania', 'continent': 'Europe', 'age': 19, 'language': 'Python' },
+  { 'firstName': 'Sou', 'lastName': 'B.', 'country': 'Japan', 'continent': 'Asia', 'age': 49, 'language': 'PHP' },
+]
+print(find_senior(list1))
