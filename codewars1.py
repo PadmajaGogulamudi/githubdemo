b24ab626b68b92e96311594bfbb7c487fce239ec
PadmajaGogulamudi <117999437+PadmajaGogulamudi@users.noreply.github.com>
2024-01-19 16:40:10 +0530

    Add files via upload

---
+++ codewars1.py
@@ -0,0 +1,15 @@
+def count_developers(lst):
+    # Your code here
+    c=0
+    for i in range(len(lst)):
+        if(lst[i]["continent"]=="Europe" and lst[i]["language"]=="JavaScript"):
+            c+=1
+    return c;
+list1 = [
+          { 'firstName': 'Noah', 'lastName': 'M.', 'country': 'Switzerland', 'continent': 'Europe', 'age': 19, 'language': 'JavaScript' },
+          { 'firstName': 'Maia', 'lastName': 'S.', 'country': 'Tahiti', 'continent': 'Oceania', 'age': 28, 'language': 'JavaScript' },
+          { 'firstName': 'Shufen', 'lastName': 'L.', 'country': 'Taiwan', 'continent': 'Asia', 'age': 35, 'language': 'HTML' },
+          { 'firstName': 'Sumayah', 'lastName': 'M.', 'country': 'Tajikistan', 'continent': 'Asia', 'age': 30, 'language': 'CSS' }
+        ]
+x=count_developers(list1)
+print(x)
